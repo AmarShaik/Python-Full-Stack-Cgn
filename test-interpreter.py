248c3a08b7a28c29757d3b76d844531c0fa6fd2b
Shaik Amar <156987603+AmarShaik@users.noreply.github.com>
2026-03-18 10:25:39 +0530

    Add files via upload

---
+++ test-interpreter.py
@@ -0,0 +1,12 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(3+3)
+6
+>>> print(8-4)
+4
+>>> print(7*6)
+42
+>>> print(6/2)
+3.0
+>>> print(6//2)
+3
