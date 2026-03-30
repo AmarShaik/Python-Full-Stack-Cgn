6301c3c4dc43fd0a537d59bae22a0726bea05a39
Shaik Amar <156987603+AmarShaik@users.noreply.github.com>
2026-03-30 10:37:00 +0530

    Add files via upload

---
+++ 10 - Tuple and Tuple Methods.py
@@ -0,0 +1,14 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Tuple
+>>> #tuple()
+>>> #It is an ORDERED and IMMUTABLE collection of elements, which are grouped together as a single unit
+>>> a = (5, 6.7, 'python', 5+6j, True, False)
+>>> type(a)
+<class 'tuple'>
+>>> len(a)
+6
+>>> a.count(True)
+1
+>>> a.index('python')
+2
