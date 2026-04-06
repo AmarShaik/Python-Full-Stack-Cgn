b4ccaea859442fcf96f960b42838c39a92302e7e
Shaik Amar <156987603+AmarShaik@users.noreply.github.com>
2026-04-06 09:30:15 +0530

    Add files via upload

---
+++ 15 - Loops 3 - range().py
@@ -0,0 +1,18 @@
+#Loops
+#range() - The range function returns a sequence of numbers, starting from 0 by default and increments by one by one and stops before a specified number
+#3 steps - start-stop-step
+'''for i in range(10):
+    print(i)'''
+
+'''for i in range(15, 36):
+    print(i)'''
+
+#Task-1
+for i in range(2, 20, 2):
+    print(i)
+
+for i in range(0, 28, 3):
+    print(i)
+
+for i in range(5, 46, 5):
+    print(i)
