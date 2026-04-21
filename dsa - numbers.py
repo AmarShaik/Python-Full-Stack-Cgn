9c5d95d7fdcb13cd814a2c97909c9ddae4b559ec
Shaik Amar <156987603+AmarShaik@users.noreply.github.com>
2026-04-21 10:16:38 +0530

    Add files via upload

---
+++ dsa - numbers.py
@@ -0,0 +1,60 @@
+'''program to print numbers form 1 to n using while loop'''
+'''n = int(input("Enter a number:"))
+loop = 1
+while loop<=n:
+   print(loop, end = ' ')
+   loop+=1'''
+
+''' dry run
+
+while loop<=n (n=5)
+while 1<=5 True   while 2<=5 True   while 3<=5 True   while 4<=5 True   while 5<=5 True   while 6<=5 False
+   loop=2               loop=3            loop=4               loop=5          loop=6           break
+   
+1  2  3  4  5'''
+
+'''program to print even numbers and odd numbers using while loop'''
+'''n = int(input("Enter a number:"))
+loop = 1 
+print("Even numbers: ")
+while loop<=n:
+   if loop%2==0:
+      print(loop, end = ' ')
+   loop+=1
+print("\n Odd numbers: ")
+i = 1
+while i<=n:
+   if i%2!=0:
+      print(i, end = ' ')
+   i+=1'''
+
+'''reverse of a number using while loop'''
+# n = int(input())
+# rev = 0
+# while n!=0:
+#    rem = n%10
+#    rev = (rev*10)+ rem
+#    n = n//10
+# print(rev)
+
+
+
+'''sum of digits of a number using while loop'''
+# n = int(input())
+# sum = 0
+# while n!=0:
+#    rem = n%10
+#    sum = sum + rem
+#    n = n//10
+# print(sum)
+
+# '''program to find out the missing number in a list of numbers
+# input = [1, 2, 3, 4, 6]
+# missing = 5'''
+# n = int(input("Enter the number of elements").split())
+
+'''program to group the words by length'''
+# a = ['hi', 'hello', 'data', 'hey']
+# for i in range(len(a)):
+#    print(len(a[i]))
+
